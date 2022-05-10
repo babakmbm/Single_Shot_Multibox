2447cf28bc4950f54ccc0a9dcae357fcb7acc82d
babakmbm <b7.hosseini@gmail.com>
2022-05-10 17:15:12 +0100

    structure

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
+import torch
+from torch.autograd import Variable
+import cv2
+import imageio
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def detect(frame, net, transform):
+
+    height, width, channel = frame.shape
+    frame_transformed = transform(frame)[0]
+    x = torch.from_numpy(frame_transformed).permute(2, 0, 1)
+    x = Variable(x.unsqueeze(0))
+    y = net(x)
+    detections = y.data
+    scale = torch.Tensor([width, height, width, height])
 
 
-# Press the green button in the gutter to run the script.
+    return frame
+
+
 if __name__ == '__main__':
-    print_hi('')
-
+    detect()
